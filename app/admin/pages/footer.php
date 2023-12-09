a971f50c3ce9aae959c52dee58a1db68b0e77c16
VincentDareda <132431423+VincentDareda@users.noreply.github.com>
2023-12-10 00:43:28 +0800

    Add files via upload

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,13 @@
+<style>
+    .main-footer {
+        background-color: #3498db; /* Kode warna CSS untuk biru yang lebih gelap */
+        color: white; /* Sesuaikan warna teks agar cocok dengan latar belakang */
+    }
+</style>
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b></b> 
+    </div>
+    <strong>BY &copy; <?= date('Y'); ?>. Kelompok 4 RPL D</strong>  .</a>
+</footer>
---
+++ app/user/pages/footer.php
@@ -0,0 +1,13 @@
+<style>
+    .main-footer {
+        background-color: #3498db; 
+        color: white; 
+    }
+</style>
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b></b> 
+    </div>
+    <strong>BY &copy; <?= date('Y'); ?>. Kelompok 4 RPL D</strong>  .</a>
+</footer>
